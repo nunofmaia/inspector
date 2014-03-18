0f0dd23cb916fb5882f69a816a128af011e5d246
Nuno Maia <nunofmaia@gmail.com>
2014-03-18 15:13:31 +0000

    Clean up code. Handle exceptions correctly.
    Since we have to use Java 6, exceptions must be on their own catch
    clause. Removed some duplicated code.

--- src/ist/meic/pa/commands/CCommand.java
+++ src/ist/meic/pa/commands/CCommand.java
@@ -5,13 +5,9 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class CCommand extends Command {
-	private Object obj;
-	private String[] args;
 
 	public CCommand(Object obj, String[] args) {
 		super(obj, args);
-		this.obj = obj;
-		this.args = args;
 	}
 	
 
@@ -26,55 +22,63 @@ public class CCommand extends Command {
 		}
 	}
 	
-	private Object handleMethodWithParams() throws IllegalAccessException, IllegalArgumentException{
+	private Object handleMethodWithParams() throws IllegalAccessException, IllegalArgumentException {
 		try {
 			Method m = getMethodByName();
 			Object[] methodParameters = getMethodParameters(m);
 			return m.invoke(obj, methodParameters);
-		} catch (NoSuchMethodException | SecurityException
-				| InvocationTargetException e) {
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+			return obj;
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return obj;
 		}
 		
 	}
 	
-	private Object handleMethod() throws IllegalAccessException, IllegalArgumentException{
+	private Object handleMethod() throws IllegalAccessException, IllegalArgumentException {
 		try {
-			Object result = obj.getClass().getDeclaredMethod(args[0])
-					.invoke(obj);
+			Object result = obj.getClass().getDeclaredMethod(args[0]).invoke(obj);
 			return result;
-		} catch (NoSuchMethodException | SecurityException|InvocationTargetException e) {
+		} catch (NoSuchMethodException e) {
 			System.err.println("Unable to fulfill request");
 			return obj;
-		} 
+		} catch (SecurityException e) {
+			System.err.println("Unable to fulfill request");
+			return obj;
+		} catch (InvocationTargetException e) {
+			System.err.println("Unable to fulfill request");
+			return obj;
+		}
 		
 	}
 
-	private Object[] getMethodParameters(Method m) throws NoSuchMethodException{
+	private Object[] getMethodParameters(Method m) throws NoSuchMethodException {
 		ArrayList<Object> methodPar = new ArrayList<Object>();
 		for (int i = 0; i < m.getParameterTypes().length; i++) {
 			Class<?> par = m.getParameterTypes()[i]; 
 			/**
 			 * A melhorar: 
-			 * 	- suporte para mais tipos de argumentos além de ints;
-			 * 			- Até agora suporta tipos primitivos: int, double, String, boolean, long e char
-			 * 	- fazer com que isto não seja um comboio de if's
+			 * 	- suporte para mais tipos de argumentos alem de ints;
+			 * 	- Ate agora suporta tipos primitivos: int, double, String, boolean, long e char
+			 * 	- fazer com que isto nao seja um comboio de if's
 			 */
 			String parValue = args[i+1];
 			if (int.class.equals(par) || int.class.equals(par)) {
 				methodPar.add(new Integer(parValue));
-			}else if(Double.class.equals(par) || double.class.equals(par)){
+			}else if(Double.class.equals(par) || double.class.equals(par)) {
 				methodPar.add(new Double(parValue));
 			} else if(String.class.equals(par)) {
 				methodPar.add(parValue);
-			} else if (Boolean.class.equals(par) || boolean.class.equals(par)){
+			} else if (Boolean.class.equals(par) || boolean.class.equals(par)) {
 				methodPar.add(new Boolean(parValue));
-			}else if (Long.class.equals(par) || long.class.equals(par)){
+			} else if (Long.class.equals(par) || long.class.equals(par)){
 				methodPar.add(new Long(parValue));
-			}else if ((Character.class.equals(par) || char.class.equals(par)) && parValue.toCharArray().length == 1){
+			} else if ((Character.class.equals(par) || char.class.equals(par)) && parValue.toCharArray().length == 1) {
 				methodPar.add(new Character(parValue.toCharArray()[0]));
-			}else{
+			} else {
 				System.err.println("Error: Unable to convert arguments");
 				throw new NoSuchMethodException();
 			}
