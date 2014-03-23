a8c4a8ea54ba06fcdd2a9c5986208b8c2ee62926
Nuno Maia <nunofmaia@gmail.com>
2014-03-23 22:13:48 +0000

    Added type checking to M command

--- src/ist/meic/pa/commands/MCommand.java
+++ src/ist/meic/pa/commands/MCommand.java
@@ -1,9 +1,15 @@
 package ist.meic.pa.commands;
 
 import ist.meic.pa.InspectionState;
+import ist.meic.pa.TypeChecking;
 import ist.meic.pa.Utils;
+import ist.meic.pa.annotations.Type;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MCommand extends Command {
 
@@ -18,22 +24,97 @@ public class MCommand extends Command {
 			Object obj = this.state.getCurrentObject();
 			String attr = this.args[0];
 			String value = this.args[1];
-			for (Field f : Utils.getAllFields(obj)) {
-				if (f.getName().equals(attr)) {
-					f.setAccessible(true);
-					Class<?> c = f.get(obj).getClass();
-					if (c.getSimpleName().equals("Integer")) {
-							 f.set(obj, Integer.parseInt(value));
-							 this.state.updateState(obj);
-					}
+			ArrayList<Field> fields = Utils.getField(obj, attr);
+			
+			if (fields.size() == 0) {
+				System.err.println("The field " + attr + " does not exist.");
+			} else if (fields.size() == 1) {
+				updateField(fields.get(0), value);
+			} else {
+				System.err.println("There are more than one field with the name " + attr + ". Please enter the number of the field you want to choose.");
+				Utils.dumpChoiceList(fields);
 
-					break;
-				}
+				handleChoice(fields, value);
 			}
+//			for (Field f : Utils.getAllFields(obj)) {
+//				if (f.getName().equals(attr)) {
+//					f.setAccessible(true);
+//					Class<?> c = f.get(obj).getClass();
+//					f.set(obj, processType(c, value));
+//					this.state.updateState(obj);
+//
+//					break;
+//				}
+//			}
 		}
 		
 		return this.state;
 
+	}
+	
+	private void updateField(Field field, String value) throws IllegalArgumentException, IllegalAccessException {
+		Object obj = this.state.getCurrentObject();
+		field.setAccessible(true);
+		Class<?> c = field.get(obj).getClass();
+		field.set(obj, processType(c, value));
+		this.state.updateState(obj);
+	}
+
+	private void handleChoice(ArrayList<Field> fields, String value) throws IllegalArgumentException, IllegalAccessException {
+		Scanner scanner = new Scanner(System.in);
+		boolean isActive = true;
+		while (isActive) {
+			System.err.print("Choice: ");
+			String[] command = scanner.nextLine().split(" ");
+
+			try {
+				int index = Integer.parseInt(command[0]);
+				if (index >= 0 && index < fields.size()) {
+					isActive = false;
+					updateField(fields.get(index), value);
+					
+				} else {
+					System.err.println("Enter a valid number between 0 and " + (fields.size() - 1) + ".");
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Enter a valid number between 0 and " + (fields.size() - 1) + ".");
+			}
+		}
+	}
+	
+	private Object processType(Class<?> type, String value) {
+		String typeName = type.getSimpleName();
+		Class<?> checker = TypeChecking.class;
+		for (Method m : checker.getDeclaredMethods()) {
+			Type t = m.getAnnotation(Type.class);
+			if (t != null) {
+				if (type.isArray()) {
+					Class<?> arrayType = type.getComponentType();
+					String[] values = value.substring(1, value.length() - 1).split(",");
+					Object arr = Array.newInstance(arrayType, values.length);
+					int index = 0;
+					for (String v : values) {
+						Array.set(arr, index, processType(arrayType, v));
+						index++;
+					}
+					return arr;
+				} else {
+					for (String v : t.value()) {
+						if (v.equals(typeName)) {
+							Object[] args = new Object[] { value };
+							try {
+								return m.invoke(null, args);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}					
+				}
+			}
+		}
+
+		return null;
 	}
 
 }
